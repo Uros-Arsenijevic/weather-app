bc5b4abd3f9464034dd21b4418107d99dd467252
Uros Arsenijevic <142681439+Uros-Arsenijevic@users.noreply.github.com>
2023-11-20 22:54:58 +0100

    Napravljen osnovni funkcional i dodate su sitne promene

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@ $('.dropDown ol').click(function(event) {
                         filterCity = filterCity.slice(0, 5);
                         $('.search-list').html("")
                         filterCity.forEach(singleElement => {
-                            let listItem = `<li>${singleElement}</li>`;
+                            let listItem = `<li><i class="fa-solid fa-map-location-dot"></i>${singleElement}</li>`;
                             $('.search-list').append(listItem)
                         })
     
@@ -109,11 +109,7 @@ $('.submitBtn').click(() => {
         if($('.search-bar > p').length === 1){
             $('.search-bar > p').remove();
         }
-        let searchCity = $('.searchLocation').val()
-        searchCity = searchCity.split(' ')
-                                .map(singleElemnet => singleElemnet[0].toUpperCase() + singleElemnet.slice(1).toLowerCase())
-                                .join(" ")
-
+        let searchCity = $('.searchLocation').val().toLowerCase();
         
         $.ajax({
             type: 'GET',
@@ -138,12 +134,12 @@ $('.submitBtn').click(() => {
                                 // add flag
                                 $('.nameCuntryAndCity img').attr({
                                     src: `https://countryflagsapi.netlify.app/flag/${singleCountry.toUpperCase()}.svg`, 
-                                    title: $('.select-country li').text()
-                                })
+                                    title: response.location.country
+                                });
                                                             
-                            }
+                            };
 
-                        })
+                        });
                     }
                 });
                 let prognoseAllDay = response.forecast.forecastday;                    
@@ -156,8 +152,67 @@ $('.submitBtn').click(() => {
                     if(index === 0){
                         $('.date p').text(`${allDayInWeek[date.getDay()]}, ${allMounth[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`)
                     }
-                    console.log($('.day-name').eq(index).text(allDayInWeek[date.getDay()]))
+
+                    // Forecast for 5 days
+                    $('.day-name').eq(index).text(allDayInWeek[date.getDay()]);
+                    $('.day-prognose > p').eq(index).text(singleDay.day.condition.text)
+                    $('.Max-temp').eq(index).html(`${Math.floor(singleDay.day.maxtemp_c)}&deg;c`);
+                    $('.Min-temp').eq(index).html(`${Math.floor(singleDay.day.mintemp_c)}&deg;c`);
+
+                });
+                $('.day-img img').each((index, singleElement) => {
+                    let dayTemp = $('.day-prognose > p').eq(index).text();
+                    changeIcons(singleElement, dayTemp)
                 })
+
+                function changeIcons(element, value){
+                    switch(value){
+                        case 'Sunny':
+                            $(element).attr('src', "img/icons/Clear-Day.svg");
+                            break;
+                        
+                        case 'Patchy rain possible':
+                        case 'Moderate rain':
+                            $(element).attr('src', "img/icons/Rain-Day.svg");
+                            break;
+                        
+                        case 'Partly cloudy':
+                            $(element).attr('src', "img/icons/FewClouds-Day.svg");
+                            break;
+                        case 'Heavy snow':
+                        case 'Patchy heavy snow':
+                        case 'Moderate snow':
+                        case 'Moderate or heavy snow showers':
+                        case 'Blowing snow':
+                        case 'Blizzard':
+                        case 'Light snow':
+                            $(element).attr('src', "img/icons/Snow-Day.svg");
+                            break;
+                        case 'Cloudy':
+                        case 'Overcast':
+                            $(element).attr('src', "img/icons/Cloudy-Day.svg");
+                            break;
+                        case 'Heavy rain':
+                            $(element).attr('src', "img/icons/Storm-Day.svg");
+                    }
+                }
+                // Adding weather details
+                $('.temperature h2').each((index, singleElement) => { 
+                    switch (index){
+                        case 1:
+                            $(singleElement).text(`${prognoseAllDay[0].day.daily_chance_of_rain}%`);
+                            break;
+                        case 2:
+                            $(singleElement).text(`${Math.floor(prognoseAllDay[0].day.maxwind_kph)}km/h`);
+                            break;
+                        case 3:
+                           $(singleElement).text(`${prognoseAllDay[0].day.avghumidity}%`);
+                            break;
+                        case 4:
+                            $(singleElement).text(prognoseAllDay[0].day.uv);
+                            break;
+                    }
+                });
 
                 // add time
                 let localTime = response.location.localtime;
@@ -165,24 +220,145 @@ $('.submitBtn').click(() => {
                 $('.time').text(localTime[1]);
 
                 // add courrend forcast
-                let currentForecast = response.current.condition.text;
-                $('.MaxAndMinTemp p:last').text(currentForecast)
+                $('.MaxAndMinTemp p:last').text(prognoseAllDay[0].day.condition.text)
 
 
                 // add Temperature
                 let temperature = response.current.temp_c;  
-                $('.temp h3').html(`${Math.round(temperature)}&deg;c`);
+                $('.temp h3, .temperature h2:first').html(`${Math.floor(temperature)}&deg;c`);
 
                 // add max and min temp
                 let maxTemp = prognoseAllDay[0].day.maxtemp_c;
                 let minTemp = prognoseAllDay[0].day.mintemp_c;
 
-                $('.MaxAndMinTemp p:first').html(`${Math.round(maxTemp)}&deg;c / ${Math.round(minTemp)}&deg;c`);
+                $('.MaxAndMinTemp p:first').html(`${Math.floor(maxTemp)}&deg;c / ${Math.floor(minTemp)}&deg;c`);
                 
 
                 // add name city and county
                 $('.nameCuntryAndCity p').text(`${response.location.name}/${response.location.country}`);
                
+
+                let time = response.location.localtime.slice(11).split(':')[0]
+                changeBackground('.cont2 img', $('.MaxAndMinTemp > p:last').text(), +time);
+
+                function changeBackground(element, value, time){
+                    switch(value){
+                        case 'Sunny':
+                            if(time >= 6 && time <= 18){
+                                $(element).attr('src', "img/icons/Clear-Day.svg");
+                                $('.background').css({
+                                    background: 'url(img/Day-backround/2.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',                           
+                                })
+                            }else{
+                                $(element).attr('src', "img/icons/Clear-Night.svg");
+                                $('.background').css({
+                                    background: 'url(img/Night-background/2.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',  
+                                })
+                            }
+                            break;
+                        
+                        case 'Patchy rain possible':
+                        case 'Moderate rain':
+                            if(time >= 6 && time <= 18){
+                                $(element).attr('src', "img/icons/Rain-Day.svg");
+                                $('.background').css({
+                                    background: 'url(img/Day-backround/5.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',                           
+                                })
+                            }else{
+                                $(element).attr('src', "img/icons/Rain-Night.svg");
+                                $('.background').css({
+                                    background: 'url(img/Night-background/5.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',  
+                                })
+                            }
+                            break;
+                        
+                        case 'Partly cloudy':
+                            if(time >= 6 && time <= 18){
+                                $(element).attr('src', "img/icons/FewClouds-Day.svg");
+                                $('.background').css({
+                                    background: 'url(img/Day-backround/4.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',                           
+                                })
+                            }else{
+                                $(element).attr('src', "img/icons/FewClouds-Night.svg");
+                                $('.background').css({
+                                    background: 'url(img/Night-background/4.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',  
+                                })
+                            }
+                            break;
+                        case 'Heavy snow':
+                        case 'Patchy heavy snow':
+                        case 'Moderate snow':
+                        case 'Moderate or heavy snow showers':
+                        case 'Blowing snow':
+                        case 'Blizzard':
+                        case 'Light snow':
+                        case 'Light sleet':
+                            if(time >= 6 && time <= 18){
+                                $(element).attr('src', "img/icons/Snow-Day.svg");
+                                $('.background').css({
+                                    background: 'url(img/Day-backround/6.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',                           
+                                })
+                            }else{
+                                $(element).attr('src', "img/icons/Snow-Night.svg");
+                                $('.background').css({
+                                    background: 'url(img/Night-background/6.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',  
+                                })
+                            }
+                            break;
+                        case 'Cloudy':
+                        case 'Overcast':
+                            if(time >= 6 && time <= 18){
+                                $(element).attr('src', "img/icons/Cloudy-Day.svg");
+                                $('.background').css({
+                                    background: 'url(img/Day-backround/3.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',                           
+                                })
+                            }else{
+                                $(element).attr('src', "img/icons/Cloudy-Night.svg");
+                                $('.background').css({
+                                    background: 'url(img/Night-background/3.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',  
+                                })
+                            }
+                            break;
+                        case 'Heavy rain':
+                            if(time >= 6 && time <= 18){
+                                $(element).attr('src', "img/icons/Storm-Day.svg");
+                                $('.background').css({
+                                    background: 'url(img/Day-backround/5.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',                           
+                                })
+                            }else{
+                                $(element).attr('src', "img/icons/Storm-Night.svg");
+                                $('.background').css({
+                                    background: 'url(img/Night-background/5.png)',
+                                    backgroundRepeat: 'no-repeat',
+                                    backgroundSize: 'cover',  
+                                })
+                            }
+                            break;
+                    }
+                }
+
 
                 $('.busca').hide()
                 $('body').css('background', 'var(--gray-900)')
